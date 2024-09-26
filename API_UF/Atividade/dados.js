2773d5c19102ee38562b0cc0df13ce5ca6b6978f
JulinBn <otaviojulio131@gmail.com>
2024-09-26 16:56:58 -0400

    up

---
+++ API_UF/Atividade/dados.js
@@ -0,0 +1,20 @@
+const colecaoAN = [
+    {
+        id: 1,
+        an: "DM",
+        nome: "Demon Slayer"
+    },
+    {
+        id: 2,
+        an: "DBZ",
+        nome: "Dragon Ball Z"
+    },
+    {
+        id: 3,
+        an: "BL",
+        nome: "Blue Lock"
+    },
+    {},
+    {},
+
+]
