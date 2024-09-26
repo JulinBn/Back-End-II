412dd396d65a3c528973a06e3714b308a371a687
JulinBn <otaviojulio131@gmail.com>
2024-09-26 16:26:32 -0400

    up

--- API_UF/index.js
+++ API_UF/index.js
@@ -3,8 +3,10 @@ const colecaoUf = require('./Dados/dados');
 
 const app = express();
 
-app.get('/ufs', (req, res) => {
-    res.json(colecaoUf)
+app.get('/ufs/:iduf', (req, res) => {
+    const idUF = parseInt(req.params.iduf);
+    const uf = colecaoUf.colecaoUf.find(item => item.id === idUF);
+    res.json(uf);
 });
 
 app.listen(8080, () => {
