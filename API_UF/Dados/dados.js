8f6d69f6069fe519241c5563cd3d13cb2b3b1252
JulinBn <otaviojulio131@gmail.com>
2024-09-26 15:41:01 -0400

    first commit

---
+++ API_UF/Dados/dados.js
@@ -0,0 +1,27 @@
+const colecaoUF = [
+    {
+        id: 1,
+        uf: "AC",
+        nome: "Acre"
+    },
+    {
+        id: 2,
+        uf:"Al",
+        nome: "ALagoas"
+    },
+    {
+        id: 3,
+        uf: "AM",
+        nome: Amazonas
+    },
+    {
+        id: 4,
+        uf: "AP",
+        nome: "Amapá"
+    },
+    {
+        id: 5,
+        uf: "BA",
+        nome: "Bahia"
+    },
+]
