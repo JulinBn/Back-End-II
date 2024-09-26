2124cbc86f5d69761923c47f8b02445d5bda3524
JulinBn <otaviojulio131@gmail.com>
2024-09-26 16:51:34 -0400

    up

--- API_UF/index.js
+++ API_UF/index.js
@@ -5,8 +5,24 @@ const app = express();
 
 app.get('/ufs/:iduf', (req, res) => {
     const idUF = parseInt(req.params.iduf);
-    const uf = colecaoUf.colecaoUf.find(item => item.id === idUF);
-    res.json(uf);
+    let mensagemErro = 'UF não encontrada';
+    let uf;
+
+    if (!(isNaN(idUF))) {
+        uf = colecaoUf.colecaoUf.find(item => item.id === idUF);
+        if (!uf) {
+            mensagemErro = 'UF não encotrada';
+        }
+} else {
+        mensagemErro = 'Requisição inválida';
+        }
+
+        if (uf) {
+            res.json(uf);
+        }   else {
+            res.status(404).json({"erro": mensagemErro});
+        }
+
 });
 
 app.listen(8080, () => {
