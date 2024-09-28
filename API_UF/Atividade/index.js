25eb77a8e647f4d4670a81cfbe4e7fb0b532c0da
JulinBn <159487778+JulinBn@users.noreply.github.com>
2024-09-28 20:33:01 -0300

    up

--- API_UF/Atividade/index.js
+++ API_UF/Atividade/index.js
@@ -2,3 +2,29 @@ const express = require(express);
 const colecaoAN = require('./dados.js');
 
 const app = express();
+
+app.get('/.ans/:idan', (req, res) => {
+    const idAN = parseInt(req.params.idan);
+    let mensagemErro = 'ANIME não encontrado';
+    let an;
+
+    if (!(isNaN(idAN))) {
+        an = colecaoAN.find(item => item.id === idAN);
+        if (!an) {
+            mensagemErro = 'ANIME não encontrado';
+
+        }
+        else{
+            mensagemErro = 'Requisacão inválida';
+        }
+        if (an) {
+            res.json(uf);
+        }else{
+            res.status(404).json({'erro': mensagemErro})
+        }
+    }
+});
+
+app.listen(8080, () => {
+    console.log('Servidor iniciado na porta 8080!')
+})
